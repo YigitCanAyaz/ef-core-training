a7d66f19056a2952c08c4f7cedfe7de8280cbb26
YigitCanAyaz <yigitcanayaz2000@gmail.com>
2023-09-11 15:18:33 +0300

    Added ORM SQL Isolation

---
+++ ORM_SQL_İzalasyonu/Models/CategorySalesFor1997.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EF2_ORM_SQL_İzalasyonu.Models
+{
+    public partial class CategorySalesFor1997
+    {
+        public string CategoryName { get; set; } = null!;
+        public decimal? CategorySales { get; set; }
+    }
+}
---
+++ ORM_SQL_İzalasyonu/Models/ProductsAboveAveragePrice.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EF2_ORM_SQL_İzalasyonu.Models
+{
+    public partial class ProductsAboveAveragePrice
+    {
+        public string ProductName { get; set; } = null!;
+        public decimal? UnitPrice { get; set; }
+    }
+}
